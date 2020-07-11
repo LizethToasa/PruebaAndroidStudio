357fc7c57a0b997f0a1c90d6d269fa947efa4416
Lizeth Toasa <lizeth.toasa@gmail.com>
2020-07-11 17:00:32 -0500

    subiendo cambios

--- app/src/main/java/com/example/pruebaandroidstudio/MainActivity.java
+++ app/src/main/java/com/example/pruebaandroidstudio/MainActivity.java
@@ -2,9 +2,15 @@ package com.example.pruebaandroidstudio;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.database.DatabaseReference;
@@ -15,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
     String nombre = et1.getText().toString();
     String correo = et2.getText().toString();
     String celular = et3.getText().toString();
+    Button btn1, btn2, btn3, btn4;
     ListView listv;
 
     FirebaseDatabase firebaseDatabase;
@@ -23,6 +30,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         et1 = (EditText) findViewById(R.id.txt1);
@@ -38,5 +46,56 @@ public class MainActivity extends AppCompatActivity {
         FirebaseApp.initializeApp(this);
         firebaseDatabase = FirebaseDatabase.getInstance();
         databaseReference = firebaseDatabase.getReference();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.icon_add:
+                if (nombre.equals("") || correo.equals("") || celular.equals("")){
+                    validacion();
+                }else{
+                    Toast.makeText(this, "Agregado", Toast.LENGTH_SHORT).show();
+                    limpiar();
+                }
+                break;
+            case R.id.icon_save:
+                Toast.makeText(this, "Guardado", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.icon_delete:
+                Toast.makeText(this, "Eliminado", Toast.LENGTH_SHORT).show();
+                break;
+            default:break;
+        }
+
+        return true;
+    }
+
+    private void limpiar() {
+        try{
+            nombre = ("");
+            correo = ("");
+            celular = ("");
+        }catch (Exception e){};
+    }
+
+    private void validacion() {
+        if(!nombre.isEmpty() && !correo.isEmpty() && !celular.isEmpty()){
+            ContentValues registro = new ContentValues();
+            registro.put("nombre", nombre);
+            registro.put("correo", correo);
+            registro.put("celular", celular);
+
+            Toast.makeText(this, "Registro exitoso", Toast.LENGTH_SHORT).show();
+
+        }else{
+            Toast.makeText(this, "LLene todos los campos", Toast.LENGTH_SHORT).show();
+        }
     }
 }
